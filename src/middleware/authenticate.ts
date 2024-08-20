295374563d05d23a267d45a5fca97f1fb858bac2
Ilham Rusydi Abd <abd.ilhamrusydi@gmail.com>
2024-08-20 14:17:39 +0700

    fix: authentication file

--- src/middleware/authenticate.ts
+++ src/middleware/authenticate.ts
@@ -1,22 +1,58 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
+import { Auth } from "../entities/auth.schema";
 
-export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+export const authMiddleware = async (req: Request, res: Response, next: NextFunction) => {
   const { accessToken, refreshToken } = req.cookies;
 
   if (!accessToken || !refreshToken) {
     return res.status(401).json({ error: "Please authenticate." });
   }
-
+  // check toke if exist
   if (accessToken) {
     try {
-      jwt.verify("accessToken", process.env.ACCESS_TOKEN_SECRET as string);
+      await jwt.verify("accessToken", process.env.JWT_ACCESS_TOKEN as string);
     } catch (error) {
+      // if false, regenerate new access token from refresh token
       if (!refreshToken) {
-        return;
+        return res.status(401).json({ error: "unauthorized" });
       }
-      console.log("refresh token expired", error);
-      return res.status(401).json({ error: "Please authenticate." });
+
+      try {
+        // check if refresh token is valid
+        jwt.verify(refreshToken, process.env.JWT_REFRESH_TOKEN as string);
+
+        // if valid, verify if it's exist in database
+        const activeRefreshToken = await Auth.findOne({ refreshToken });
+
+        if (!activeRefreshToken) {
+          console.log("refresh token not found in db");
+          return res.json({ message: "please re-login" });
+        }
+
+        const payload = jwt.decode(refreshToken) as {
+          id: string;
+          name: string;
+          email: string;
+        };
+        console.log(payload);
+        const newAccessToken = jwt.sign(
+          {
+            id: payload.id,
+            name: payload.name,
+            email: payload.email,
+          },
+          process.env.JWT_ACCESS_TOKEN as string,
+          { expiresIn: 300 }
+        );
+
+        return res.cookie("accessToken", newAccessToken, { httpOnly: true }).json({ data: "getting data.." });
+        // regenerate new access token
+      } catch (error) {
+        // if invalid, user need to re-login
+        return res.status(401).json({ message: "please Re-login.." });
+      }
     }
   }
+  next();
 };
